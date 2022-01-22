e47b8d2b53232d8b9c5ee4adb01e2a8ffcf06e1f
Ihor <Igorryd6@gmail.com>
2022-01-22 22:43:43 +0200

    Скрипт который вычисляет сколько лет пользователю

--- getAge.php
+++ getAge.php
@@ -1,10 +1,11 @@
 <?php
 //Реализол скрипт который выводит сколько лет пользователю.
 function getAge($y, $m, $d){
-    if($m > date('m') || $m == date('m') && $d > date('d')){
+    if(($m > date('m') or $m == date('m')) && $d > date('d')){
         return (date('Y') - $y - 1);
     }
     else{
         return (date('Y') - $y);
     }
 }
+echo getAge(2005,01,12);//17
