59464705438669dc6013b26455b0530e872a9f4f
Ihor <Igorryd6@gmail.com>
2022-01-23 21:52:40 +0200

    поиск гороскопа по числу месяца и дню рождения

--- getZodiacSign.php
+++ getZodiacSign.php
@@ -6,4 +6,4 @@ function getZodiacalSign($month, $day) {
    $signsstart = array(1=>21, 2=>20, 3=>20, 4=>20, 5=>20, 6=>20, 7=>21, 8=>22, 9=>23, 10=>23, 11=>23, 12=>23);
    return $day < $signsstart[$month + 1] ? $signs[$month - 1] : $signs[$month % 12];
 }
-echo getZodiacalSign(1, 12);
+echo getZodiacalSign(1, 12);//Capricorn that`s true!
