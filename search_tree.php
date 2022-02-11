d9610f4232956de04757247c23fa7df05836fa96
Ihor <Igorryd6@gmail.com>
2022-02-11 20:59:51 +0200

    Дерево директории на php

---
+++ search_tree.php
@@ -0,0 +1,21 @@
+<?php
+function showTree($folder, $space)
+{
+    /* Получаем полный список файлов и каталогов внутри $folder */
+    $files = scandir($folder);
+    foreach ($files as $file) {
+        /* Отбрасываем текущий и родительский каталог */
+        if (($file == '.') || ($file == '..')) continue;
+        $f0 = $folder . '/' . $file; //Получаем полный путь к файлу
+        /* Если это директория */
+        if (is_dir($f0)) {
+            /* Выводим, делая заданный отступ, название директории */
+            echo $space . $file . "<br />";
+            /* С помощью рекурсии выводим содержимое полученной директории */
+            showTree($f0, $space . '&nbsp;&nbsp;');
+        }else {
+            echo $space . $file . "<br />";
+        }
+    }
+}
+showTree("./", "");
